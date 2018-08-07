0ccc4ed266b7809d957caa37598a6e91134a0a17
Jan Vennemann <jan.vennemann@gmx.net>
2018-08-07 14:19:04 +0200

    style(tslint): setup linting using tslint

--- templates/typescript-alloy-app/app/controllers/index.ts
+++ templates/typescript-alloy-app/app/controllers/index.ts
@@ -9,33 +9,33 @@ const window = $.getTopLevelViews()[0] as Titanium.UI.Window;
 
 // Typed and documented controller methods are available as usual under $
 $.addListener(nameInput, 'change', e => {
-	greetingLabel.text = greeter.sayHello(e.value);
+  greetingLabel.text = greeter.sayHello(e.value);
 });
 
 function decrement(): void {
-	greeter.decrement();
-	updateGreeting(nameInput.value);
+  greeter.decrement();
+  updateGreeting(nameInput.value);
 }
 
 function increment(): void {
-	greeter.increment();
-	updateGreeting(nameInput.value);
+  greeter.increment();
+  updateGreeting(nameInput.value);
 }
 
 function updateGreeting(name: string) {
-	let greeting = '';
-	if (name === '') {
-		greeting = 'Please enter your name first!';
-	} else {
-		greeting = greeter.sayHello(name);
-	}
-	greetingLabel.text = greeting;
+  let greeting = '';
+  if (name === '') {
+    greeting = 'Please enter your name first!';
+  } else {
+    greeting = greeter.sayHello(name);
+  }
+  greetingLabel.text = greeting;
 }
 
 if (OS_ANDROID) {
-	window.activity.onStart = () => {
-		window.activity.actionBar.hide();
-	}
+  window.activity.onStart = () => {
+    window.activity.actionBar.hide();
+  };
 }
 
 window.open();
