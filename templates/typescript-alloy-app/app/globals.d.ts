5cce60108436c642a6b1bdaf4d7945f03c68d592
Mathias Lorenzen <mathias.lorenzen@broelstaerk.dk>
2019-10-28 15:10:10 +0100

    improved strong-typing of alloy apps.

--- templates/typescript-alloy-app/app/globals.d.ts
+++ templates/typescript-alloy-app/app/globals.d.ts
@@ -2,18 +2,13 @@ type NonFunctionPropertyNames<T> = { [K in keyof T]: T[K] extends Function ? nev
 type NonFunctionProperties<T> = Pick<T, NonFunctionPropertyNames<T>>;
 type Dictionary<T> = Partial<NonFunctionProperties<T>>;
 
-/**
- * Reference to the current controller instance.
- */
-declare const $: AlloyController;
+declare interface JQueryStatic extends AlloyController {
+}
 
 /**
  * The base class for Alloy controllers.
  */
-declare interface AlloyController {
-
-  [k: string]: any;
-
+declare interface AlloyController extends Backbone.Model {
   /**
    *
    * @param proxy View object to which to add class(es).
